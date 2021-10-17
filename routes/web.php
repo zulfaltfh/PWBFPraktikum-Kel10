e4bd007b0519986ff5890b8c4aebc28e8aeb57c9
Zulfaltfh <78287902+zulfaltfh@users.noreply.github.com>
2021-10-17 20:43:53 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -92,10 +92,71 @@ Route::get('/update_Ukuran_pegawai/{id}',		'App\Http\Controllers\UkuranControlle
 //hapus Ukuran
 Route::get('/destroy_Ukuran_pegawai/{id}',		'App\Http\Controllers\UkuranController@destroy');
 
+//Kota
+Route::get('/Kota_pegawai',					'App\Http\Controllers\KotaController@home');
+//input Kota 
+Route::get('/create_Kota_pegawai',				'App\Http\Controllers\KotaController@create');
+Route::get('/input_Kota_pegawai',				'App\Http\Controllers\KotaController@insert');
+//update Kota untuk pegawai
+Route::post('/edit_Kota_pegawai/{id}',			'App\Http\Controllers\KotaController@edit');
+Route::get('/update_Kota_pegawai/{id}',		'App\Http\Controllers\KotaController@update');
+//hapus Kota
+Route::get('/destroy_Kota_pegawai/{id}',		'App\Http\Controllers\KotaController@destroy');
 
+//Role
+Route::get('/Role_pegawai',					'App\Http\Controllers\RoleController@home');
+//input Role 
+Route::get('/create_Role_pegawai',				'App\Http\Controllers\RoleController@create');
+Route::get('/input_Role_pegawai',				'App\Http\Controllers\RoleController@insert');
+//update Role untuk pegawai
+Route::post('/edit_Role_pegawai/{id}',			'App\Http\Controllers\RoleController@edit');
+Route::get('/update_Role_pegawai/{id}',		'App\Http\Controllers\RoleController@update');
+//hapus Role
+Route::get('/destroy_Role_pegawai/{id}',		'App\Http\Controllers\RoleController@destroy');
 
+//Supplier
+Route::get('/Supplier_pegawai',					'App\Http\Controllers\SupplierController@home');
+//input Supplier 
+Route::get('/create_Supplier_pegawai',				'App\Http\Controllers\SupplierController@create');
+Route::get('/input_Supplier_pegawai',				'App\Http\Controllers\SupplierController@insert');
+//update Supplier untuk pegawai
+Route::post('/edit_Supplier_pegawai/{id}',			'App\Http\Controllers\SupplierController@edit');
+Route::get('/update_Supplier_pegawai/{id}',		'App\Http\Controllers\SupplierController@update');
+//hapus Supplier
+Route::get('/destroy_Supplier_pegawai/{id}',		'App\Http\Controllers\SupplierController@destroy');
 
+//Penerimaan
+Route::get('/Penerimaan_pegawai',					'App\Http\Controllers\PenerimaanController@home');
+//input Penerimaan 
+Route::get('/create_Penerimaan_pegawai',				'App\Http\Controllers\PenerimaanController@create');
+Route::get('/input_Penerimaan_pegawai',				'App\Http\Controllers\PenerimaanController@insert');
+//update Penerimaan untuk pegawai
+Route::post('/edit_Penerimaan_pegawai/{id}',			'App\Http\Controllers\PenerimaanController@edit');
+Route::get('/update_Penerimaan_pegawai/{id}',		'App\Http\Controllers\PenerimaanController@update');
+//hapus Penerimaan
+Route::get('/destroy_Penerimaan_pegawai/{id}',		'App\Http\Controllers\PenerimaanController@destroy');
 
+//Pembayaran
+Route::get('/Pembayaran_pegawai',					'App\Http\Controllers\PembayaranController@home');
+//input Pembayaran 
+Route::get('/create_Pembayaran_pegawai',				'App\Http\Controllers\PembayaranController@create');
+Route::get('/input_Pembayaran_pegawai',				'App\Http\Controllers\PembayaranController@insert');
+//update Pembayaran untuk pegawai
+Route::post('/edit_Pembayaran_pegawai/{id}',			'App\Http\Controllers\PembayaranController@edit');
+Route::get('/update_Pembayaran_pegawai/{id}',		'App\Http\Controllers\PembayaranController@update');
+//hapus Pembayaran
+Route::get('/destroy_Pembayaran_pegawai/{id}',		'App\Http\Controllers\PembayaranController@destroy');
+
+//History Stock
+Route::get('/HistoryStock_pegawai',					'App\Http\Controllers\HistoryStockController@home');
+//input HistoryStock 
+Route::get('/create_HistoryStock_pegawai',				'App\Http\Controllers\HistoryStockController@create');
+Route::get('/input_HistoryStock_pegawai',				'App\Http\Controllers\HistoryStockController@insert');
+//update HistoryStock untuk pegawai
+Route::post('/edit_HistoryStock_pegawai/{id}',			'App\Http\Controllers\HistoryStockController@edit');
+Route::get('/update_HistoryStock_pegawai/{id}',		'App\Http\Controllers\HistoryStockController@update');
+//hapus HistoryStock
+Route::get('/destroy_HistoryStock_pegawai/{id}',		'App\Http\Controllers\HistoryStockController@destroy');
 
 
 
