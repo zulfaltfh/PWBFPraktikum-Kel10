65bb3c92b12a960da4742238a3d6a253873502e3
Zulfaltfh <78287902+zulfaltfh@users.noreply.github.com>
2021-10-31 22:59:53 +0700

    update view layout Pegawai

--- resources/views/layout/layoutpegawai.blade.php
+++ resources/views/layout/layoutpegawai.blade.php
@@ -9,10 +9,14 @@
         <meta name="author" content="" />
         <title> Dashboard </title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="css/styles.css?v=echo time();" rel="stylesheet" />
-        <link href="css/form.css?v=echo time();" rel="stylesheet" />
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet"  href="css/styles.css"/>
+        {{-- <link href="css/form.css?v=echo time();" rel="stylesheet" /> --}}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
+
+
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
@@ -57,69 +61,21 @@
                             
 
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-align-left"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-file-alt"></i></div>
                                 Data Master
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="{{ url('/homePegawai') }}">Barang</a>
                                     <a class="nav-link" href="{{ url('/Kota_pegawai') }}">Kota User</a>
                                     <a class="nav-link" href="{{ url('/Role_pegawai') }}">Role User</a>
-                                    <a class="nav-link" href="{{ url('/homePegawai') }}">Barang</a>
                                     <a class="nav-link" href="{{ url('/Jenis_Barang_Pegawai') }}">Jenis Barang</a>
                                     <a class="nav-link" href="{{ url('/pemesanan_pegawai') }}">Pemesanan</a>
                                     <a class="nav-link" href="{{ url('/pembayaran_pegawai') }}">Pembayaran</a>
-                                    <a class="nav-link" href="{{ url('/HistoryStock_pegawai') }}">Stock Barang</a>
-                                    <a class="nav-link" href="{{ url('/pemesanan_pegawai') }}">Detail Barang</a>
-                                    <a class="nav-link" href="{{ url('/pemesanan_pegawai') }}">Detail Pemesanan</a>
-                                    <a class="nav-link" href="{{ url('/pemesanan_pegawai') }}">Detail Penerimaan</a>
                                 </nav>
                             </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Pages
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Authentication
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="login.html">Login</a>
-                                            <a class="nav-link" href="register.html">Register</a>
-                                            <a class="nav-link" href="password.html">Forgot Password</a>
-                                        </nav>
-                                    </div>
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Error
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="401.html">401 Page</a>
-                                            <a class="nav-link" href="404.html">404 Page</a>
-                                            <a class="nav-link" href="500.html">500 Page</a>
-                                        </nav>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
                         </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
                     </div>
                 </nav>
             </div>
@@ -130,7 +86,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                            <div class="text-muted">Copyright &copy; Herrys Aghista & Zulfa Lutfiah 2021</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
@@ -144,8 +100,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
         <script src="js/datatables-simple-demo.js"></script>
     </body>
--- resources/views/pegawai/insert_barang.blade.php
+++ resources/views/pegawai/insert_barang.blade.php
@@ -5,41 +5,47 @@
     <div class="container-fluid px-4">
         <h1 class="mt-4">Input Barang Baru</h1>
     </div>
-    <form class="b" action = "{{ url('/create_barang_pegawai') }}" method = "post">
-	<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-	<table>
-		<tr>
-			<td>Nama Barang</td>
-			<td><input type="text" name='nama_bar'/></td>
-		</tr>
-		<tr>
-			<td>Jenis Barang</td>
-			<td>
-				<select name="id_jb">
-					@foreach($table_jenis_barang as $item)
-				<option name="id_jb" value="{{ $item->id }}">{{ $item->jenis_barang }}</option>
-					@endforeach
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>Stok Barang</td>
-			<td><input type="text" name='stock_barang'/></td>
-		</tr>
-		<tr>
-			<td>Harga Beli</td>
-			<td><input type="text" name='harga_beli_bar'/></td>
-		</tr>
-		<tr>
-			<td>Harga Jual</td>
-			<td><input type="text" name='harga_jual_bar'/></td>
-		</tr>
-		<tr>
-			<td colspan = '2'>
-			<input type = 'submit' value = "Input Data"/></td>
-		</tr>
-	</table>
-</form>
+    <form action = "{{ url('/create_barang_pegawai') }}" method = "post">
+		@csrf
+
+		 <div class="content">
+			 <div class="row">
+				 
+			 </div>
+		 </div>
+		<table>
+			<tr>
+				<td>Nama Barang</td>
+				<td><input type="text" name='nama_bar'/></td>
+			</tr>
+			<tr>
+				<td>Jenis Barang</td>
+				<td>
+					<select name="id_jb">
+						@foreach($table_jenis_barang as $item)
+					<option name="id_jb" value="{{ $item->id }}">{{ $item->jenis_barang }}</option>
+						@endforeach
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Stok Barang</td>
+				<td><input type="text" name='stock_barang'/></td>
+			</tr>
+			<tr>
+				<td>Harga Beli</td>
+				<td><input type="text" name='harga_beli_bar'/></td>
+			</tr>
+			<tr>
+				<td>Harga Jual</td>
+				<td><input type="text" name='harga_jual_bar'/></td>
+			</tr>
+			<tr>
+				<td colspan = '2'>
+				<input type = 'submit' value = "Input Data"/></td>
+			</tr>
+		</table>	
+	</form>
 </main>
 
 @endsection
