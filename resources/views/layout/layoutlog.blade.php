ac9ac36b10a0dbc097280e30f8a562054df86bb4
Zulfaltfh <78287902+zulfaltfh@users.noreply.github.com>
2021-11-03 00:13:39 +0700

    ubah view login

--- resources/views/layout/layoutlog.blade.php
+++ resources/views/layout/layoutlog.blade.php
@@ -66,21 +66,23 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>Sistem Informasi Pengadaan Barang Fancy Boutique</title>
+    <title>Login | SI Fancy Boutique</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link rel="stylesheet" href="css/login.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Custom CSS-->
+    <link rel="stylesheet" href="css/loginn.css">
 
   </head>
 
-  <body class="text-center">
+  <body>
     
-            @yield('log')
-          
-    
-
-
+    @yield('log')
+  
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!--Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,42 +1,40 @@
 @extends('layout/layoutlog')
 
 @section('log')
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="title" style="align-content: center">
-          <h2>Sistem Informasi Pengadaan Barang</h2>
-          <h2>Fancy Boutique</h2>
-        </div>
-        <div class="col-md-5">
-          <main class="form-signin">
-            <h1 class="h4 mb-3 fw-normal">Login</h1>
-            <form action="{{ url('/a') }}" method="POST">
-              @csrf
-              <div class="form-floating">
-                <input type="text" name="session" value="1" hidden="">
-                <input type="text" name="username" class="form-control" id="username" autofocus required>
-                <label for="floatingInput">Username</label>
-              </div>
-              <br>
-              <div class="form-floating">
-                <input type="password" name="id" class="form-control" id="password" placeholder="Password" required>
-                <label for="floatingPassword">Password (sementara password di ganti id)</label>
-              </div>
-    
-              <div class="checkbox mb-3">
-                <label>
-                  <input type="checkbox" value="remember-me"> Remember me
-                </label>
-              </div>
-              <button class="w-100 btn btn-lg " style="background-color: #ea4c88" type="submit">Login</button><br>
-              <br><p style="color:white">Belum punya akun ? Silahkan <a href="{{ url('/input_User_pegawai') }}">registrasi</a></p>
-              
-            </form>
-          </main>
-  
-        </div>
-      </div>
-    </div>
-    
 
-    @endsection
+    <div class="global-container">
+        <div class="card login-form">
+            <div class="card-body">
+                <h2 class="card-title text-center">SISTEM INFORMASI</h2>
+                <h2 class="card-title text-center">FANCY BOUTIQUE</h2>
+            </div>
+            <div class="card-text mt-2">
+                <form action="{{ url('/a') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="session" value="1" hidden="">
+                        <input type="text" name="username" class="form-control" id="username" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password">Password</label>
+                        <input type="password" name="id" class="form-control" id="password" required>
+                    </div>
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" value="remember-me">
+                      <label class="form-check-label">Remember me</label>
+                    </div>
+
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+
+                    <br>
+                    <center>
+                        <p style="color:#242424">Belum punya akun ? Silahkan <a href="{{ url('/input_User_pegawai') }}">registrasi</a></p>
+                    </center>
+                  </form>
+            </div>
+        </div>
+    </div>
+@endsection
